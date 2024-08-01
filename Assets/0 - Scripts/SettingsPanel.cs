d3f95c44fd6373312958049493babcade5fd5025
Malik Muhammad Arslan <malikarslan6162aus@gmail.com>
2024-08-01 16:49:25 +1000

    Death Panel Fixed & Settings Panels Features Added

--- Assets/0 - Scripts/SettingsPanel.cs
+++ Assets/0 - Scripts/SettingsPanel.cs
@@ -10,6 +10,18 @@ public class SettingsPanel : MonoBehaviour
     [SerializeField] Slider musicSwitch;
     [SerializeField] AudioClip gamePlayAudio;
     [SerializeField] TextMeshProUGUI versionText;
+ 
+    string fbURL = "https://www.facebook.com/profile.php?viewas=100000686899395&id=61556956161259";
+    string instaURL = "https://www.instagram.com/aspire_games_studio/?igsh=dGZhemd3bGpiNHh0";
+    string discoedURL = "https://discord.gg/bMmsU8k8";
+
+
+
+    string appstoreUrl = "https://apps.apple.com/app/castle-invaders/id6480585969";
+    string appstoreMoreGamesUrl = "https://apps.apple.com/app/castle-invaders/id6480585969";
+
+    string googlePlayStoreUrl = "https://play.google.com/store/apps/details?id=com.AspirePlay.RetroAdventurer&pcampaignid=web_share";
+    string googlePlayStoreMoreGamesUrl = "https://play.google.com/store/apps/details?id=com.AspirePlay.RetroAdventurer&pcampaignid=web_share";
 
     void Start()
     {
@@ -31,7 +43,47 @@ public class SettingsPanel : MonoBehaviour
     }
 
   
-    
+    public void OpenFaceBook()
+    {
+        Application.OpenURL(fbURL);
+    }
+    public void OpenInstagram()
+    {
+        Application.OpenURL(instaURL);
+    }
+
+    public void OpendDiscord()
+    {
+        Application.OpenURL(discoedURL);
+    }
+
+    public void RateUs()
+    {
+        if (Application.platform != RuntimePlatform.IPhonePlayer)
+        {
+            // Open Google Play Store URL
+            Application.OpenURL(googlePlayStoreUrl);
+        }
+        else
+        {
+            // Open Apple App Store URL
+            Application.OpenURL(appstoreUrl);
+        }
+    }
+
+    public void MoreGames()
+    {
+        if (Application.platform != RuntimePlatform.IPhonePlayer)
+        {
+            // Open Google Play Store URL
+            Application.OpenURL(googlePlayStoreMoreGamesUrl);
+        }
+        else
+        {
+            // Open Apple App Store URL
+            Application.OpenURL(appstoreMoreGamesUrl);
+        }
+    }
 
 }
 
