8fca9b8d13f785eeb89e8bc9d72504fc8c332ed7
HAIDER MUKHTAR <76527973+HaiderMukhtar1@users.noreply.github.com>
2024-09-02 14:11:53 +0500

    Revert "Updated Rewarded Video Ad"
    This reverts commit 88c065feb8de0348a2598e0605a3342d2b82d1b7.

--- Assets/0 - Scripts/Admob Ads/Rewarded.cs
+++ Assets/0 - Scripts/Admob Ads/Rewarded.cs
@@ -1,36 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds;
 using GoogleMobileAds.Api;
 using System;
-using UnityEngine;
 
-public class Rewarded : Singelton<Rewarded>
+public class Rewarded : MonoBehaviour
 {
-    protected override void Awake()
-    {
-        base.Awake();
-    }
 
-   
-    public void Start()
-    {
-         MobileAds.Initialize((InitializationStatus initStatus) =>  { });
-    }
-
-
- #if UNITY_ANDROID
-    private string _adUnitId = "ca-app-pub-1387627577986386/4685752798";
-#elif UNITY_IPHONE
-  private string _adUnitId = "ca-app-pub-3940256099942544/1712485313";
-#else
-  private string _adUnitId = "unused";
-#endif
+    public static Rewarded Instance;
 
 
     RewardedAd _rewardedAd;
+ 
+      void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    void Start()
+    {
+         MobileAds.Initialize((InitializationStatus initStatus) =>  {});
+          LoadRewardedAd();
+     }
 
+
+ #if UNITY_ANDROID
+      string _adUnitId = "ca-app-pub-1387627577986386/4685752798";
+#elif UNITY_IPHONE
+    string _adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+    string _adUnitId = "unused";
+#endif
+     
+
+   
     public void LoadRewardedAd()
     {
-        // Clean up the old ad before loading a new one.
-        if (_rewardedAd != null)
+         if (_rewardedAd != null)
         {
             _rewardedAd.Destroy();
             _rewardedAd = null;
@@ -38,15 +47,12 @@ public class Rewarded : Singelton<Rewarded>
 
         Debug.Log("Loading the rewarded ad.");
 
-        // create our request used to load the ad.
-        var adRequest = new AdRequest();
+         var adRequest = new AdRequest();
 
-        // send the request to load the ad.
-        RewardedAd.Load(_adUnitId, adRequest,
+         RewardedAd.Load(_adUnitId, adRequest,
             (RewardedAd ad, LoadAdError error) =>
             {
-                // if error is not null, the load request failed.
-                if (error != null || ad == null)
+                 if (error != null || ad == null)
                 {
                     Debug.LogError("Rewarded ad failed to load an ad " +
                                    "with error : " + error);
@@ -57,61 +63,56 @@ public class Rewarded : Singelton<Rewarded>
                           + ad.GetResponseInfo());
 
                 _rewardedAd = ad;
-                ShowRewardedAd();
-            });
+             });
     }
 
     public void ShowRewardedAd()
-    {  
+    {         
         if (_rewardedAd != null && _rewardedAd.CanShowAd())
         {
             _rewardedAd.Show((Reward reward) =>
             {
-                // TODO: Reward the user.
-              //  RegisterEventHandlers(_rewardedAd);
-             //   RegisterReloadHandler(_rewardedAd);
+                //currencyManager.GetInstance().IncreaseCoins(200);
+                //currencyManager.SaveCurrencyData();
 
-                //Assigning Reward After Ad
+                //Add Up reward.
                 if (AdmobRewardedVideo.Instance)
                 {
                     AdmobRewardedVideo.Instance.RewardAfterAd();
                 }
-                               
+
+                RegisterEventHandlers(_rewardedAd);
+                RegisterReloadHandler(_rewardedAd);
+
             });
         }
     }
 
-    void RegisterEventHandlers(RewardedAd ad)
+      void RegisterEventHandlers(RewardedAd ad)
     {
-        // Raised when the ad is estimated to have earned money.
-        ad.OnAdPaid += (AdValue adValue) =>
+         ad.OnAdPaid += (AdValue adValue) =>
         {
             Debug.Log(String.Format("Rewarded ad paid {0} {1}.",
                 adValue.Value,
                 adValue.CurrencyCode));
         };
-        // Raised when an impression is recorded for an ad.
-        ad.OnAdImpressionRecorded += () =>
+         ad.OnAdImpressionRecorded += () =>
         {
             Debug.Log("Rewarded ad recorded an impression.");
         };
-        // Raised when a click is recorded for an ad.
-        ad.OnAdClicked += () =>
+         ad.OnAdClicked += () =>
         {
             Debug.Log("Rewarded ad was clicked.");
         };
-        // Raised when an ad opened full screen content.
-        ad.OnAdFullScreenContentOpened += () =>
+         ad.OnAdFullScreenContentOpened += () =>
         {
             Debug.Log("Rewarded ad full screen content opened.");
         };
-        // Raised when the ad closed full screen content.
-        ad.OnAdFullScreenContentClosed += () =>
+         ad.OnAdFullScreenContentClosed += () =>
         {
             Debug.Log("Rewarded ad full screen content closed.");
         };
-        // Raised when the ad failed to open full screen content.
-        ad.OnAdFullScreenContentFailed += (AdError error) =>
+         ad.OnAdFullScreenContentFailed += (AdError error) =>
         {
             Debug.LogError("Rewarded ad failed to open full screen content " +
                            "with error : " + error);
@@ -120,22 +121,20 @@ public class Rewarded : Singelton<Rewarded>
 
       void RegisterReloadHandler(RewardedAd ad)
     {
-        // Raised when the ad closed full screen content.
-        ad.OnAdFullScreenContentClosed += () =>
-        {
+         ad.OnAdFullScreenContentClosed += () =>
+    {
             Debug.Log("Rewarded Ad full screen content closed.");
 
-            // Reload the ad so that we can show another as soon as possible.
-            LoadRewardedAd();
+             LoadRewardedAd();
         };
-        // Raised when the ad failed to open full screen content.
-        ad.OnAdFullScreenContentFailed += (AdError error) =>
+         ad.OnAdFullScreenContentFailed += (AdError error) =>
         {
             Debug.LogError("Rewarded ad failed to open full screen content " +
                            "with error : " + error);
 
-            // Reload the ad so that we can show another as soon as possible.
-            LoadRewardedAd();
+             LoadRewardedAd();
         };
     }
+
+
 }
