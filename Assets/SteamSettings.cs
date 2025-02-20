5e802a8a340756a4cc874604c670d6c731bf0ef0
Malik Muhammad Arslan <malikarslan6162aus@gmail.com>
2025-02-20 17:00:38 +1100

    Steam Works SDK Added

---
+++ Assets/SteamSettings.cs
@@ -0,0 +1,21 @@
+using Steamworks;
+using UnityEngine;
+
+public class SteamSettings : MonoBehaviour
+{
+
+    [SerializeField] string testName;
+    void Start()
+    {
+        if(!SteamManager.Initialized)
+        {
+            return;
+        }
+        string name = SteamFriends.GetPersonaName();
+        testName = name;
+        Debug.Log(name);
+      
+    }
+
+   
+}
